ecc8d202e5fd952e0f1d632979728d91aeb9ae22
mayank-sen <45895238+mayank-sen@users.noreply.github.com>
2020-05-21 14:04:30 +0530

    Final Solution

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -1,12 +1,12 @@
 class BinaryTree {
-	// Root of the Binary Tree
+
 	Node root;
 
 	public BinaryTree() {
 		root = null;
 	}
 
-	/* function to print level order traversal of tree */
+	
 	void printLevelOrder() {
 		int h = height(root);
 		int i;
@@ -14,15 +14,12 @@ class BinaryTree {
 			printGivenLevel(root, i);
 	}
 
-	/*
-	 * Compute the "height" of a tree -- the number of nodes along the longest path
-	 * from the root node down to the farthest leaf node.
-	 */
+	
 	int height(Node root) {
 		if (root == null)
 			return 0;
 		else {
-			/* compute height of each subtree */
+			
 			int lheight = height(root.left);
 			int rheight = height(root.right);
 
@@ -34,7 +31,7 @@ class BinaryTree {
 		}
 	}
 
-	/* Print nodes at the given level */
+	
 	void printGivenLevel(Node root, int level) {
 		if (root == null)
 			return;
@@ -46,7 +43,7 @@ class BinaryTree {
 		}
 	}
 
-	/* Driver program to test above functions */
+	
 	public static void main(String args[]) {
 		BinaryTree tree = new BinaryTree();
 		tree.root = new Node(1);
